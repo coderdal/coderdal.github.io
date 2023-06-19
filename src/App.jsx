8b6782cd01e5803c0bed9f039c63759049eb40ea
coderdal <agircoder@gmail.com>
2023-06-19 16:22:37 +0300

    react routed installed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import Header from "@/components/Header.jsx";
 
 import styles from "@/styles/app.module.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    //homepage-wildcard route
+    path: "*",
+    element: <div>home page!</div>,
+  },
+  {
+    //homepage route
+    path: "/projects",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 function App() {
   return (
     <main className={styles.container}>
       <Header />
+      <RouterProvider router={router} />
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import styles from "@/styles/header.module.css";
 
 function Header() {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>me.</div>
+      <nav></nav>
+    </header>
+  );
 }
 
 export default Header;
