d0450fc2cd932a7161f4137d665f04a7f10d81ba
coderdal <agircoder@gmail.com>
2023-06-19 15:31:39 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header.jsx";
+
+import styles from "@/styles/app.module.css";
+
+function App() {
+  return (
+    <main className={styles.container}>
+      <Header />
+    </main>
+  );
+}
+
+export default App;
