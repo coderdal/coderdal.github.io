c227de8cf75c8aa9c872a0e5c425c8f5496811c0
coderdal <agircoder@gmail.com>
2023-06-23 11:31:52 +0300

    contact page design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,42 @@
 import styles from "@/styles/contact.module.css";
 
+import { IoMdMail, IoLogoLinkedin } from "react-icons/io";
+import { ImGithub, ImStackoverflow } from "react-icons/im";
+
+import { useTranslation } from "react-i18next";
+
 function Contact() {
-  return <section className={styles.section}>sa</section>;
+  const { t } = useTranslation();
+
+  return (
+    <section className={styles.section}>
+      <h2 className={styles.contactTitle}>{t("contactTitle")}</h2>
+      <p className={styles.contactDescription}>{t("contactDescription")}</p>
+      <p className={styles.contactDescription}>E-mail: erdalcodes@gmail.com</p>
+      <div className={styles.socialLinks}>
+        <a href="mailto:erdalcodes@gmail.com" target="_blank" rel="noreferrer">
+          <IoMdMail />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/muhammederdal/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IoLogoLinkedin />
+        </a>
+        <a href="https://github.com/coderdal" target="_blank" rel="noreferrer">
+          <ImGithub />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/15223969/coderdal"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ImStackoverflow />
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
